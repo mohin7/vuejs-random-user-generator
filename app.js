1b96d1bd44a70d5e727ead84736b72596e7740eb
csemohin <csemohin@gmail.com>
2021-04-25 14:28:52 +0600

    Update loading button
    Signed-off-by: csemohin <csemohin@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      isLoading: false,
       firstName: "John",
       lastName: "Doe",
       email: "john@email.com",
@@ -10,13 +11,18 @@ const app = Vue.createApp({
   },
   methods: {
     async getUser() {
+      this.isLoading = true;
       const res = await fetch("https://randomuser.me/api");
       const { results } = await res.json();
-      (this.firstName = results[0].name.first),
+
+      if(results){
+        this.isLoading = false;
+        (this.firstName = results[0].name.first),
         (this.lastName = results[0].name.last),
         (this.email = results[0].email),
         (this.gender = results[0].gender),
         (this.picture = results[0].picture.large);
+      }
     },
   },
 });
