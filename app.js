dae2b67497622c787471f6bd4e0bd10bf26a9310
csemohin <csemohin@gmail.com>
2021-04-25 13:45:22 +0600

    Added vue functionality
    Signed-off-by: csemohin <csemohin@gmail.com>

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const app = Vue.createApp({
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
   },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      (this.firstName = results[0].name.first),
+        (this.lastName = results[0].name.last),
+        (this.email = results[0].email),
+        (this.gender = results[0].gender),
+        (this.picture = results[0].picture.large);
+    },
+  },
 });
 
 app.mount("#app");
